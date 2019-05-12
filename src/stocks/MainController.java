82fcc53d1628c24b4ed214da9f91b8061c6d2897
Zaheen <hasan_zaheen@yahoo.com>
2019-05-12 17:56:07 -0400

    Update for issues

--- src/stocks/MainController.java
+++ src/stocks/MainController.java
@@ -29,7 +29,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
 
 
 public class MainController extends Application{
